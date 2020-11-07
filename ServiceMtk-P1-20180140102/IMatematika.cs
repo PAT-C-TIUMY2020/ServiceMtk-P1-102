a05d59f08d9990d38586de3b68fa31c9620be14d
Mochammad Reza Rafli Zamzam Alfarizi <rezarafli9668@gmail.com>
2020-11-07 14:26:44 +0700

    Pertemuan 6

--- ServiceMtk-P1-20180140102/IMatematika.cs
+++ ServiceMtk-P1-20180140102/IMatematika.cs
@@ -7,21 +7,35 @@ using System.Text;
 
 namespace ServiceMtk_P1_20180140102
 {
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IMatematika
     {
-         
+
+
         [OperationContract]         
+        [FaultContract(typeof(MathFault))]
         /* output->*/
         int Tambah(int a, int b); //method
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class 
     }
 
@@ -41,5 +55,7 @@ namespace ServiceMtk_P1_20180140102
             get { return _y; }
             set { _y = value; }
         }
+
+
     }
 }
--- ServiceMtk-P1-20180140102/Matematika.cs
+++ ServiceMtk-P1-20180140102/Matematika.cs
@@ -12,23 +12,86 @@ namespace ServiceMtk_P1_20180140102
     {
         public int Bagi(int a, int b)
         {
-            return a / b;
+            try
+            {
+                return a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
-        public int Kali(int a, int b) { return a * b; }
+        public int Kali(int a, int b) 
+        {
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
 
-        public int Kurang(int a, int b) { return a - b; }
+        public int Kurang(int a, int b) 
+        {
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
 
-        public int Tambah(int a, int b) { return a + b; }
+        public int Tambah(int a, int b)
+        {
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
 
         //TKoordinat:nama methodnya, a,b = input seperti int.         
         //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya dengan yb lalu dimasukkan ke hasil         
         public Koordinat TKoordinat(Koordinat a, Koordinat b)         
-        {             
-            Koordinat hasil = new Koordinat();             
-            hasil.X = a.X + b.X;             
-            hasil.Y = a.Y + b.Y;             
-            return hasil;         
+        {
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
     }
 }
